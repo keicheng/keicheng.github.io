967ab24a9fefb8723e31896d6b86ed0f89fbfbc0
Kei <info@kei.tw>
2016-10-09 23:45:18 +0800

    adjust phone layout

--- fuckhours/js/fuckhours.js
+++ fuckhours/js/fuckhours.js
@@ -1,7 +1,19 @@
 $(function() {
     var $cells = $('.cells'),
+        $twTable = $('#twTable'),
+        $twCell = $twTable.children('tbody'),
         exchangeRate = 31.4,
-        tw;
+        $select = $('.dataSelect'),
+        isTW,
+        tw = {
+            country: '台灣',
+            hours: 2104,
+            gni: 20925,
+            year: 2015,
+            pvd: 7,
+            pph: 12,
+            eng: 'Taiwan'
+        };
 
     $.extend({
         moneyFormat: function(str) {
@@ -13,27 +25,64 @@ $(function() {
                 return $.moneyFormat(str.substr(0, str.length - 3)) + ',' + str.substr(str.length - 3);
             }
         }
-    })
+    });
+
+    var t_workday = 365 - (tw.pvd + tw.pph + 104),
+        t_hd = tw.hours / t_workday,
+        tt = tw.pvd + tw.pph;
 
     $.each(data, function(index, value) {
-        tw = (value.country == '台灣') ? 'class="mom-im-here"' : '';
+        isTW = (value.eng == 'Taiwan') ? 'class="mom-im-here"' : '';
 
-        var workday = 365 - (value.pvd + value.pph + 104),
-            h_d = value.hours / workday;
+        var v_workday = 365 - (value.pvd + value.pph + 104),
+            v_hd = value.hours / v_workday,
+            vt = value.pvd + value.pph;
 
-        $cells.append('<tr id="fh_' + index + '" '+ tw +'>'
-            + '<td></td>'
-            + '<td>' + value.eng + '<br /><small>' + value.country + '</small></td>'
-            + '<td>' + value.year + '</td>'
-            + '<td>' + $.moneyFormat(value.hours) + ' <small>(' + (index+1) + ')</small>' + '</td>'
-            + '<td>' + (Math.round(h_d * 10) / 10) + '</td>'
-            + '<td>' + $.moneyFormat(value.gni * exchangeRate) + '</td>'
-            + '<td>' + $.moneyFormat(Math.round(value.gni / value.hours * exchangeRate )) + '</td>'
-            + '<td>' + value.pvd + '</td>'
-            + '<td>' + value.pph + '</td>'
-            + '<td>' + (value.pvd + value.pph + 104) + '</td>'
-            + '</tr>');
+        if (value.eng != 'Taiwan') {
+            $cells.append('<tr id="fh_' + index + '" '+ isTW +'/>');
+
+            var $newTr = $('#fh_' + index);
+
+            $newTr.append('<td class="hide-xs" />');
+            $newTr.append('<td data-label="國家">' + value.eng + ' <br class="hide-xs" /><small>' + value.country + '</small></td>');
+            $newTr.append('<td data-label="工時資料年份">' + value.year + '</td>');
+            $newTr.append('<td class="' + ((tw.hours >= value.hours) ? ((tw.hours > value.hours) ? 'success' : 'warning') : 'error') + '" data-label="工時(工時排名)">' + $.moneyFormat(value.hours) + ' <small>(' + (index+1) + ')</small>' + '</td>');
+            $newTr.append('<td class="' + ((t_hd >= v_hd) ? ((t_hd > v_hd) ? 'success' : 'warning') : 'error') + '" data-label="單日平均工時">' + (Math.round(v_hd * 10) / 10) + '</td>');
+            $newTr.append('<td class="' + ((tw.gni >= value.gni) ? ((tw.gni > value.gni) ? 'error' : 'warning') : 'success') + '" data-label="人均收入(NTD)">' + $.moneyFormat(value.gni * exchangeRate) + '</td>');
+            $newTr.append('<td class="' + ((tw.gni >= value.gni) ? ((tw.gni > value.gni) ? 'error' : 'warning') : 'success') + '" data-label="時薪(NTD)">' + $.moneyFormat(Math.round(value.gni / value.hours * exchangeRate )) + '</td>');
+            $newTr.append('<td class="' + ((tw.pvd >= value.pvd) ? ((tw.pvd > value.pvd) ? 'error' : 'warning') : 'success') + '" data-label="年假天數">' + value.pvd + '</td>');
+            $newTr.append('<td class="' + ((tw.pph >= value.pph) ? ((tw.pph > value.pph) ? 'error' : 'warning') : 'success') + '" data-label="國定假日">' + value.pph + '</td>');
+            $newTr.append('<td class="' + ((tt >= vt) ? ((tt > vt) ? 'error' : 'warning') : 'success') + '" data-label="假日總數">' + (vt + 104) + '</td>');
+
+        } else {
+            $cells.append('<tr id="fh_' + index + '" '+ isTW +'>'
+                + '<td class="hide-xs"></td>'
+                + '<td data-label="國家">' + value.eng + ' <br class="hide-xs" /><small>' + value.country + '</small></td>'
+                + '<td data-label="工時資料年份">' + value.year + '</td>'
+                + '<td data-label="工時(工時排名)">' + $.moneyFormat(value.hours) + ' <small>(' + (index+1) + ')</small>' + '</td>'
+                + '<td data-label="單日平均工時">' + (Math.round(v_hd * 10) / 10) + '</td>'
+                + '<td data-label="人均收入(NTD)">' + $.moneyFormat(value.gni * exchangeRate) + '</td>'
+                + '<td data-label="時薪(NTD)">' + $.moneyFormat(Math.round(value.gni / value.hours * exchangeRate )) + '</td>'
+                + '<td data-label="年假天數">' + value.pvd + '</td>'
+                + '<td data-label="國定假日">' + value.pph + '</td>'
+                + '<td data-label="假日總數">' + (vt + 104) + '</td>'
+                + '</tr>');
+        }
+
+        $select.append('<option value="' + index + '">'+ value.eng + ' ' + value.country + '</option>');
     });
+
+    $twCell.append('<tr>'
+        + '<td data-label="國家">' + tw.eng + ' <br class="hide-xs" /><small>' + tw.country + '</small></td>'
+        + '<td data-label="工時資料年份">' + tw.year + '</td>'
+        + '<td data-label="工時(工時排名)">' + $.moneyFormat(tw.hours) + ' <small>(6)</small>' + '</td>'
+        + '<td data-label="單日平均工時">' + (Math.round(t_hd * 10) / 10) + '</td>'
+        + '<td data-label="人均收入(NTD)">' + $.moneyFormat(tw.gni * exchangeRate) + '</td>'
+        + '<td data-label="時薪(NTD)">' + $.moneyFormat(Math.round(tw.gni / tw.hours * exchangeRate )) + '</td>'
+        + '<td data-label="年假天數">' + tw.pvd + '</td>'
+        + '<td data-label="國定假日">' + tw.pph + '</td>'
+        + '<td data-label="假日總數">' + (tt + 104) + '</td>'
+        + '</tr>');
 
     var $table = $('#myTable'),
         $thr = $('.thr');
@@ -63,6 +112,21 @@ $(function() {
             } else {
                 _this.addClass('sorting_good');
             }
+        }
+    });
+
+    $select.on('change', function(e) {
+        var _this = $(this),
+            val = _this.val(),
+            $target = $('#fh_' + val);
+
+        $cells.children('tr').removeClass('active');
+        $target.addClass('active');
+
+        if (val == 5) {
+            $twTable.hide();
+        } else {
+            $twTable.show();
         }
     });
 });
